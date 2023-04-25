f44f417181e39e641ebfb4c91dde42bdcd29a71b
marco76tv <marco.sottana@gmail.com>
2023-04-25 16:42:49 +0200

    .

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'production' => false,
+    'baseUrl' => '',
+    'title' => 'Jigsaw',
+    'description' => 'Website description.',
+    'collections' => [],
+];
